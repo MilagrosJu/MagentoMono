31548ee876f5056b02878ada441ff7150ef032f8
Milagros <milagros.soria@vml.com>
2024-10-03 18:13:28 -0500

    automatizacion completada Mono

--- src/test/java/ExcuteTest.java
+++ src/test/java/ExcuteTest.java
@@ -17,56 +17,14 @@ public class ExcuteTest {
         String url = "https://tiendaonline.movistar.com.pe/ofertas/servicioshogar/magento-mono";
         String celular = "999999999";
         String dni = "99999999";
-<<<<<<< HEAD
-=======
-        String horario = "2024-10-03 10:00:00"; //Se debe actualizar la fecha
->>>>>>> 1c12bbd3e491364a0717e897c4668da98ee07a60
 
         webSite.navigateToWebsite(url);
-<<<<<<< HEAD
+
         //Banner Principal
         webSite.validatePoliticasBanner("https://centrodetransparencia.movistar.com.pe/politica-local-privacidad", "BANNER POLÍTICAS Y PRIVACIDAD");
         webSite.openPromociones();
         webSite.validatePromocionesBanner("https://centrodetransparencia.movistar.com.pe/derechos", "BANNER PROMOCIONES Y NOVEDADES");
         webSite.closePromociones();
-=======
-//        webSite.validate20DTO("https://centrodetransparencia.movistar.com.pe/politica-local-privacidad", "POPUP 20% DTO"); // Replace with the link URL
-//        webSite.selectLegal20DTO();
-//        webSite.inputFormulatio20DTO(celular, dni);
-//        WebSite.selectHorario20DTO(horario);
-//        webSite.submitPedido20DTO();
-//        String mensaje20DTO = webSite.validatePedido20STO();
-//        if (mensaje20DTO.equals("¡Gracias por contactarnos!")) {
-//            System.out.println("Prueba exitosa: Se verificó el mensaje de confirmación M20%");
-//        } else {
-//            System.out.println("Prueba fallida: El mensaje de confirmación no es el esperado");
-//        }
-//        webSite.closeModal20DTO();
-
-        // Popup DELIVERY
-        webSite.validateTerminosDelivery("https://www.movistar.com.pe/movil/postpago/portabilidad?utm_source=%28direct%29&amp;utm_medium=%28none%29&amp;utm_campaign=%28direct%29&amp;utm_content=undefined&amp;utm_term=undefined&amp;gclid=&amp;_gl=1*14w1h6w*_gcl_au*MTYxOTMzODg0OS4xNzIxMjM3NzA5*_ga*NDkzMTU1NjI0LjE3MjEyMzc3MDk.*_ga_8Q7MD06RY0*MTcyMTMzMTE0MS43LjEuMTcyMTMzMTE2Ni4zNS4wLjA.#portlet_com_liferay_journal_content_web_portlet_JournalContentPortlet_INSTANCE_jb7ddqEUWB7j", "POPUP DELIVERY"); // Replace with the link URL
-        webSite.openFormularioDelivery();
-        webSite.validatePoliticas("https://centrodetransparencia.movistar.com.pe/politica-local-privacidad", "POPUP DELIVERY"); // Replace with the link URL
-        webSite.selectPromNove();
-        webSite.selectRestricciones();
-        webSite.inputFormulario(celular, dni);
-        WebSite.selectHorario(horario);
-        webSite.submitPedido();
-        String mensajeDelivery = webSite.validarPedido();
-        if (mensajeDelivery.equals("¡Gracias por contactarnos!")) {
-            System.out.println("Prueba exitosa: Se verificó el mensaje de confirmación MDELIVERY");
-        } else {
-            System.out.println("Prueba fallida: El mensaje de confirmación no es el esperado");
-        }
-        webSite.closeModal();
-        webSite.closeModalDelivery();
-
-        // Banner
-        //webSite.cerrarModales(); //Quitar en grupo
-        webSite.selecB1Bullet(1);
-        webSite.validatePoliticasBanner("https://centrodetransparencia.movistar.com.pe/politica-local-privacidad", "BANNER"); // Replace with the link URL
-        webSite.selectPromNoveBanner();
->>>>>>> 1c12bbd3e491364a0717e897c4668da98ee07a60
         webSite.inputFormularioBanner(celular, dni);
         webSite.submitLoquiero();
         webSite.validarEnvioFormulario();
